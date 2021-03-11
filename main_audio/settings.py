10fae991b3b830ae4d5516f9f4ebe7860b3027dd
Vladimir Baranov <vtgk12ak@gmail.com>
2021-03-11 07:53:23 +0500

    commit add

--- main_audio/settings.py
+++ main_audio/settings.py
@@ -126,7 +126,8 @@ STATIC_URL = '/static/'
 # STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [STATIC_DIR]
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 django_heroku.settings(locals())
